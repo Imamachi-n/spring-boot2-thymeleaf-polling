073725d921144486723b32e3adee62b0715bddc0
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-01-30 21:53:12 +0900

    create docType selection and iteration.

--- src/main/java/com/imamachi/simplepolling/controller/QuestionNewController.java
+++ src/main/java/com/imamachi/simplepolling/controller/QuestionNewController.java
@@ -1,5 +1,6 @@
 package com.imamachi.simplepolling.controller;
 
+import com.imamachi.simplepolling.form.QuestionDetailForm;
 import com.imamachi.simplepolling.form.QuestionForm;
 import com.imamachi.simplepolling.form.QuestionRootForm;
 import com.imamachi.simplepolling.model.Question;
@@ -21,10 +22,29 @@ public class QuestionNewController {
         // 初期処理
         QuestionRootForm questionRootForm = new QuestionRootForm();
         ArrayList<QuestionForm> questionForms = new ArrayList<>();
-        questionForms.add(new QuestionForm(Question.DocType.singleQ, true));
-        questionForms.add(new QuestionForm(Question.DocType.multiQ, true));
-        questionForms.add(new QuestionForm(Question.DocType.commentQ, false));
-        QuestionForm teste = questionForms.get(0);
+
+        ArrayList<QuestionDetailForm> questionDetailForms = new ArrayList<>();
+        questionDetailForms.add(new QuestionDetailForm());
+        questionDetailForms.add(new QuestionDetailForm());
+        questionDetailForms.add(new QuestionDetailForm());
+
+        ArrayList<QuestionDetailForm> questionDetailForms2 = new ArrayList<>();
+        questionDetailForms2.add(new QuestionDetailForm());
+        questionDetailForms2.add(new QuestionDetailForm());
+        questionDetailForms2.add(new QuestionDetailForm());
+        questionDetailForms2.add(new QuestionDetailForm());
+        questionDetailForms2.add(new QuestionDetailForm());
+
+        QuestionForm questionForm1 = new QuestionForm(Question.DocType.singleQ, true);
+        questionForm1.setQuestionDetails(questionDetailForms);
+        QuestionForm questionForm2 = new QuestionForm(Question.DocType.multiQ, true);
+        questionForm2.setQuestionDetails(questionDetailForms2);
+        QuestionForm questionForm3 = new QuestionForm(Question.DocType.commentQ, false);
+        questionForm3.setQuestionDetails(questionDetailForms);
+
+        questionForms.add(questionForm1);
+        questionForms.add(questionForm2);
+        questionForms.add(questionForm3);
         questionRootForm.setQuestions(questionForms);
 
         model.addAttribute("questionRootForm", questionRootForm);
@@ -33,10 +53,11 @@ public class QuestionNewController {
 
 //    @RequestParam(name = "submit") String submit,
     @PostMapping("/questionNew")
-    public String postQuestion(@Validated @ModelAttribute QuestionRootForm questionRootForm,
+    public String postQuestion(@RequestParam(name = "submit") String submit,
+                               @Validated @ModelAttribute QuestionRootForm questionRootForm,
                                BindingResult result,
                                Model model){
-//        System.out.println(submit);
+        System.out.println(submit);
 //        String docType = submit.split("_")[0];
 //        String index = submit.split("_")[1];
         model.addAttribute("questionRootForm", questionRootForm);
