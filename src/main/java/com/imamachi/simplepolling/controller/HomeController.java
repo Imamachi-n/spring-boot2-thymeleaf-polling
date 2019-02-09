a3ceab203b5a94b5f6747425d7e9eda6e83321cc
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-02-09 23:21:13 +0900

    re-creating a top page template

--- src/main/java/com/imamachi/simplepolling/controller/HomeController.java
+++ src/main/java/com/imamachi/simplepolling/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.imamachi.simplepolling.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +11,13 @@ public class HomeController {
 
     @GetMapping("")
     public String getHomePage(){
+        // 現在のアンケートを取得
+
+        return "/home";
+    }
+
+    @PostMapping("")
+    public String setQuestionnaire(){
         return "/home";
     }
 }
